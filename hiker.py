18cd69a4dffb823e820e339ed2f011052137f57f
nataliaberaki <naber1286@oslomet.no>
2025-04-19 23:48:48 +0200

    first fail test

--- hiker.py
+++ hiker.py
@@ -1,1 +1,4 @@
-
+#simple test that should fail
+def to_roman(number):
+    if number == 4:
+        return "IV"
--- test_hiker.py
+++ test_hiker.py
@@ -1,1 +1,11 @@
+from hiker import to_roman
 
+#simple tests
+def test_1_to_I():
+    assert to_roman(1) == "I"
+
+def test_2_to_II():
+    assert to_roman(2) == "II"
+    
+def test_4_to_IV():
+    assert to_roman(4) == "IV"
